9d41b93d2a2473e69cc38e3666af8806bd0b6e37
lilianadav <145382867+lilianadav@users.noreply.github.com>
2024-04-28 22:10:56 -0400

    Add files via upload

---
+++ fruitloops/cereal.js
@@ -0,0 +1,42 @@
+var tileCount = 20;
+var actRandomSeed = 0;
+
+var circleAlpha = 130;
+var circleColor;
+
+function setup() {
+  createCanvas(8000, 8000);
+  noFill();
+  circleColor = color(0, 0, 0, circleAlpha);
+  randomSeed(random(100000));
+}
+
+function draw() {
+  translate(width / tileCount / 2, height / tileCount / 2);
+
+  background(255);
+
+  randomSeed(actRandomSeed);
+
+  strokeWeight(mouseY / 60);
+
+  for (var gridY = 0; gridY < tileCount; gridY++) {
+    for (var gridX = 0; gridX < tileCount; gridX++) {
+
+      var posX = width / tileCount * gridX;
+      var posY = height / tileCount * gridY;
+
+      var shiftX = random(-mouseX, mouseX) / 20;
+      var shiftY = random(-mouseX, mouseX) / 20;
+
+    
+      var strokeColor = color(random(255), random(255), random(255), circleAlpha);
+
+      
+      stroke(strokeColor);
+      noFill(); 
+
+      ellipse(posX + shiftX, posY + shiftY, mouseY / 15, mouseY / 15);
+    }
+  }
+}
